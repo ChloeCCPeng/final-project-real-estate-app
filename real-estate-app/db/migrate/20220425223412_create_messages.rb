941ba7221487bbe70170bc13afabc7c40e90e21e
Chloe <chloepeng331@icloud.com>
2022-04-25 19:33:39 -0400

    create models

--- real-estate-app/db/migrate/20220425223412_create_messages.rb
+++ real-estate-app/db/migrate/20220425223412_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.string :conversation
+      t.text :conversation
 
       t.timestamps
     end

cc882cfb7894eae64072f491c95eea3efcaadfef
Chloe <chloepeng331@icloud.com>
2022-04-27 00:28:45 -0400

    messages routes setup

--- real-estate-app/app/controllers/houses_controller.rb
+++ real-estate-app/app/controllers/houses_controller.rb
@@ -7,18 +7,15 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     render json: House.all
   end
 
-
   def create
     house = House.create!(house_params)
     render json: house, status: created
   end
 
-
   def show
     house = find_house
     render json: house
   end
-
 
   def update
     house = find_house
@@ -26,33 +23,28 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     render json: house, status: updated
   end
 
-
   def destroy
     house = find_house
     house.destroy
   end
 
-
+  
   private
   
   def house_params
     params.permit(:address, :lotSizeAcres, :lotSizeSquareFeet, :listPrice, :bathroomsTotal, :bedroomsTotal)
   end
 
-
   def find_house
     House.find(params[:id])
   end
-
 
   def render_not_found
     render json: {error: "Hey, I can't find any house"}, status: 404
   end
 
-
   def render_unprocessable_entity invalid
     render json: {errors: invalid.record.errors.full_messages}, status:422
   end
-
 
 end
